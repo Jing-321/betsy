85c3187aeaf0a485145fbc419f3bc5c3dd3842ff
Yingjing Lin <yjlin789@gmail.com>
2020-11-18 10:08:41 -0800

    relate order to user

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_item
 
-  validates :user, presence: true
+  validates :user_id, presence: true
   validates :status, presence: true, inclusion: {in: ["open", "completed"]}
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_065001) do
+ActiveRecord::Schema.define(version: 2020_11_18_180632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user"
-    t.string "status", default: "open"
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
 end
