caf888bcea536e5fa5139d09bc7315db203a1f76
dnsrocha <drocha@gmail.com>
2020-11-18 22:15:38 -0800

    building actions for order_items controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,51 +1,59 @@
 class OrderItemsController < ApplicationController
   before_action :find_product, only: :create
 
-  # def create
-  #   new_id = params["product_id"]
-  #   new_qty = params["quantity"]
-  #
-  #   if session[:order_id] == nil || session[:order_id] == false || !session[:order_id]
-  #     @order = Order.create(status: "pending")
-  #     session[:order_id] = @order.id
-  #   else
-  #     @order = Order.find_by(id: session[:order_id])
-  #   end
-  #
-  #   if new_qty.to_i + @order.current_qty(new_id) > Product.find(new_product_id).stock #check order model for "current quantity"
-  #     flash.now[:status] = :danger
-  #     flash.now[:result_text] = "Error: please check product availability."
-  #     render 'products/main', status: :bad_request
-  #     return
-  #   end
-  #
-  # end
+  def create
+
+  end
 
   # def update
-  #   @order_item.quantity = params[:new_quantity]
-  #   @order_item.save
-  #   flash[:status] = :success
-  #   flash[:result_text] = "Cart updated."
-  #   redirect_to order_path(session[:order_id])
+  #   new_item = @order_item.product
+  #   qty = order_item_params[:quantity].to_i
+  #
+  #   if qty > new_item.stock
+  #     flash[:status] = :failure
+  #     flash[:result_text] = "#{new_item.name} is low in stock. Cannot add item to cart."
+  #     redirect_to order_path
+  #     return
+  #   else
+  #     @order_item.update(order_item_params)
+  #     flash[:status] = :success
+  #     flash[:result_text] = "#{new_item.name} added to cart."
+  #     redirect_to order_path
+  #     return
+  #   end
   # end
 
-  def update
-    new_item = @order_item.product
-    qty = order_item_params[:quantity].to_i
+  def increase_quantity
 
-    if qty > new_item.stock
-      flash[:status] = :failure
-      flash[:result_text] = "#{new_item.name} is low in stock. Cannot add item to cart."
-      redirect_to cart_path
-      return
-    else
-      @order_item.update(order_item_params)
-      flash[:status] = :success
-      flash[:result_text] = "#{new_item.name} added to cart."
-      redirect_to cart_path
-      return
+    product = Product.find_by!(id: params[:product_id]).stock #please check
+
+    session[:order].each do |item|
+      if item["product_id"] == params["product_id"].to_i && item["quantity"] < product
+        item["quantity"] += 1
+        flash[:success] = "#{product[:name]} added to shopping cart."
+      elsif item["product_id"] == params["product_id"].to_i && item['quantity'] == product
+        flash[:error] = "#{product[:name]} is low in stock. No additional units can be added to cart."
+      end
     end
+
+    redirect_to order_items_path
+    return
   end
+
+
+  def decrease_quantity
+    session[:order].each do |item|
+      current_item = Product.find(item["product_id"])
+      if current_item.quantity == 0
+        flash[:error] = "Something is wrong. There is no #{current_item.name} in your cart."
+        redirect_back(fallback_location: order_items_path)
+        return
+      end
+
+      if item["product_id"] == params['format'].to_i
+        item["quantity"] > 1 ? item["quantity"] -= 1 : session[:cart].delete(item)
+      end
+    end
 
   def delete_item  #destroy
     @order_item.destroy
