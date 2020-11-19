adb2dcb24455b077bc53ff1ca886a3e4bdafa927
Yingjing Lin <yjlin789@gmail.com>
2020-11-18 22:39:23 -0800

    set up OAuth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,56 +2,35 @@ class UsersController < ApplicationController
 
   before_action :find_user, only: [:show]
 
-  # TBD - What are we doing for  login => Authentication or Authorization?
 
-  # def login_form
-  #   @user = User.new
-  # end
-  # ==> Do we need a login_form html.erb file?
   def create
-    @user = User.find_by(username: params[:username])
-
-    if guest?
-      @user = guest_user
-      session[:user_id] = @user.id
-      session[:guest_user_id] = nil
-      redirect_to user_path(@user)
-    elsif @user == nil
-      flash[:error] = 'Username does not exist'
-      redirect_to login_path
+    auth_hash = request.env["omniauth.auth"]
+    user = User.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if user
+      flash[:success] = "Welcome back #{user.username}!"
     else
-      if @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
+      user = User.build_from_github(auth_hash)
+      if user.save
+        flash[:success] = "Logged in as new user #{user.username}."
       else
-        flash[:error] = 'Username and Password do not match'
-        redirect_to login_path
+        flash[:error] = "Sorry, could not create new account #{user.errors.message}"
+        return redirect_to root_path
       end
-    end
-  end
-
-  def login
-    user = User.find_by(username: params[:user][:username])
-    # New User
-    if user.nil?
-      user = User.new(username: params[:user][:username])
-      unless user.save
-        flash[:error] = 'Unexpected error occured. Login unsuccessful.'
-        redirect_to root_path
-        return
-      end
-      flash[:welcome] = "Welcome #{user.username}"
-    else
-      # Existing User
-      flash[:welcome] = "Welcome back #{user.username}"
     end
 
     session[:user_id] = user.id
-    redirect_to root_path
+    return redirect_to root_path
+  end
+
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out. See you next time!"
+    return redirect_to root_path
   end
 
   def index
-    @users = User.all
+    @users = User.where.not("uid = nil")
   end
 
   def show
@@ -68,23 +47,6 @@ class UsersController < ApplicationController
       redirect_to root_path
       return
     end
-  end
-
-  def logout
-    if session[:user_id]
-      user = User.find_by(id: session[:user_id])
-      if user.nil?
-        session[:user_id] = nil
-        flash[:notice] = 'Error! Unknown User'
-      else
-        session[:user_id] = nil
-        flash[:notice] = "Goodbye #{user.username}"
-      end
-    else
-      flash[:error] = 'You must be logged in to logout'
-      redirect_to root_path
-    end
-    redirect_to root_path
   end
 
   def create_guest
