9349ca21e1956a0bac9538ea8c7d42cff1a67ec0
Jasmine <jasylop@gmail.com>
2020-11-19 08:45:16 -0800

    added tests for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,12 @@
 class CategoriesController < ApplicationController
 
-  before_action :find_category, only: [:show]
+  before_action :find_category, only: [:show, :destroy]
 
   def index
     @categories = Category.all
   end
 
-  def show
-    if @category.nil?
-      flash[:error]
-      redirect_back fallback_location: root_path
-    end
-  end
+  def show; end
 
   def new
     @category = Category.new
@@ -22,23 +17,35 @@ class CategoriesController < ApplicationController
     if @category.save
       flash[:success] = "Successfully created #{@category.name}"
       # not sure if this will redirect to where user was prior to the form
-      # where will user add category? user show page?
+      # todo where will user add category? user show page?
       redirect_back fallback_location: root_path
       return
     else
-      flash[:error] = "Could not create new category: #{@category.errors.message}"
+      flash[:error] = "Could not create new category: #{@category.errors.messages}"
       render :new, status: :bad_request
       return
     end
   end
 
+  def destroy
+    @category.delete
+    flash[:success] = "#{@category.name} was successfully deleted"
+    return redirect_back fallback_location: root_path
+  end
+
+
   private
 
   def category_params
-    return params.require(:work).permit(:name)
+    return params.require(:category).permit(:name)
   end
 
   def find_category
     @category = Category.find_by(id: params[:id])
+    if @category.nil?
+      # flash[:error]
+      return render file: "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+    end
+    return @category
   end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,83 @@
 require "test_helper"
 
 describe CategoriesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "responds with success when getting index" do
+      get categories_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "responds with success with a valid id" do
+      fun = categories(:fun)
+      get category_path(fun)
+      must_respond_with :success
+    end
+
+    it "returns not found with an invalid id" do
+      get category_path(-1)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "new" do
+    it "responds with success when getting new form" do
+      get new_category_path
+      must_respond_with :success
+    end
+  end
+
+  describe "create" do
+    let(:new_category) {
+      {
+        category: {
+          name: "test name"
+        }
+      }
+    }
+    it "successfully creates a category" do
+      expect {
+        post categories_path, params: new_category
+      }.must_differ 'Category.count', 1
+
+      must_respond_with :redirect
+      expect(flash[:success]).wont_be_nil
+
+      updated_cat = Category.find_by(name: "test name")
+
+      expect(updated_cat.name).must_equal new_category[:category][:name]
+    end
+
+    it "will not create product if params are invalid" do
+      new_category[:category][:name] = nil
+
+      expect {
+        post categories_path, params: new_category
+      }.wont_differ 'Category.count'
+
+      expect(flash[:error]).wont_be_nil
+    end
+  end
+
+  describe "destroy" do
+    it "succesfully destroys category with valid id" do
+      romantic = categories(:romantic)
+      p romantic
+      expect {
+        delete category_path(romantic)
+      }.must_differ "Category.count", -1
+
+      must_respond_with :redirect
+      expect(flash[:success]).wont_be_nil
+    end
+
+    it "responds with not_found given an invalid id" do
+      expect {
+        delete category_path(-1)
+      }.wont_differ "Category.count"
+
+      must_respond_with :not_found
+    end
+  end
 end
