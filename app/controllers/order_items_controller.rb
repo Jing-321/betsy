076444ae9650c676cd4bc9708d6c805e092666ea
dnsrocha <drocha@gmail.com>
2020-11-19 23:17:53 -0800

    updates to order_items controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,27 +3,58 @@ class OrderItemsController < ApplicationController
   # before_action :validate_quantity, only: [:create, :update]
 
   def create
+    # @book = Book.new(book_params)
+    #
+    # if @book.save
+    #   redirect_to :action => 'list'
+    # else
+    #   @subjects = Subject.all
+    #   render :action => 'new'
+    # end
 
+    qty = params["quantity"].to_i
+    new_item = params["product_id"]  #Product.find_by(id: params["product_id"])
+
+    if session[:order_id] == nil
+      @order = Order.new(status: "pending")
+      session[:order_id] = @order.id
+    else
+      @order.find_by(id: session[:order_id])
+    end
+
+    session[:order].each do |item|
+      if item["product_id"] == new_item.id
+        if item["quantity"] < new_item.stock
+          item["quantity"] += 1
+          flash[:sucess] = "#{item.name} added to cart."
+          redirect_to order_path
+          return
+        elsif item["quantity"] == new_item.stock
+          flash[:error] = "#{item.name} is low in stock. Can't add to the cart."
+          redirect_to order_path
+          return
+        end
+      end
+    end
+
+    #def update
+
+    @order_item = OrderItem.new(quantity: qty, product_id: new_item.id, id: @order.id)
+    if @order_item.save
+      session[:order] << @order_item     #array ?
+      flash[:success] = "#{new_item.name} added to the cart."
+      redirect_to product_path(new_item)
+      return
+    else
+      flash.now[:error] = "Something is wrong. Can't add #{Product.find_by(id: @order_item.product_id).name}"
+      render :new, status: :bad_request
+      return
+    end
+
+    flash[:error] = "#{new_item.name} is low in stock. Can't add to the cart."
+    #end
 
   end
-
-  # def update
-  #   new_item = @order_item.product
-  #   qty = order_item_params[:quantity].to_i
-  #
-  #   if qty > new_item.stock
-  #     flash[:status] = :failure
-  #     flash[:result_text] = "#{new_item.name} is low in stock. Cannot add item to cart."
-  #     redirect_to order_path
-  #     return
-  #   else
-  #     @order_item.update(order_item_params)
-  #     flash[:status] = :success
-  #     flash[:result_text] = "#{new_item.name} added to cart."
-  #     redirect_to order_path
-  #     return
-  #   end
-  # end
 
   def increase_quantity #work in progress
 
@@ -82,6 +113,8 @@ class OrderItemsController < ApplicationController
   def order_item_params
     return params.require(:order_item).permit(:quantity, :product_id, :order_id)
   end
-end
+  end
+
+
 
 
