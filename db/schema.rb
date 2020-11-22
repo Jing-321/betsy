0012317b43152deda0ce52dd4d1a5afa7bb4512e
dnsrocha <drocha@gmail.com>
2020-11-21 21:18:59 -0800

    removed column status from products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_050706) do
+ActiveRecord::Schema.define(version: 2020_11_22_051654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2020_11_22_050706) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo_url"
-    t.string "status"
     t.bigint "user_id"
     t.boolean "active"
     t.index ["user_id"], name: "index_products_on_user_id"
