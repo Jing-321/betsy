d7d2f63cf4a755196b26fbab905d8de38042ce16
Yingjing Lin <yjlin789@gmail.com>
2020-11-22 23:50:45 -0800

    add test for logout

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,20 @@
 require "test_helper"
 
 describe UsersController do
+  # describe "create" do
+  #   it "will returns 200 for a logged-in user" do
+  #     perform_login
+  #
+  #     get
+  #   end
+  #
+  # end
+
   describe "index" do
     it "can list users with listing products" do
-
+      get "/products"
+      must_respond_with :success
+      ####how???
     end
 
     it "will not list users without listing products" do
@@ -11,18 +22,19 @@ describe UsersController do
     end
   end
 
-  describe "create" do
-
-  end
-
   describe "destroy" do
     it "can logout current user" do
-      session[:user_id] = users(:jing)
-      expect{
-        delete users_path
-      }.wont_change 'User.count'
+      perform_login
+      expect(session[:user_id]).wont_be_nil
+      delete logout_path
+      expect(session[:user_id]).must_be_nil
+      must_redirect_to root_path
+    end
+  end
 
-      expect(session[:user_id]).must_equal nil
+  describe "show" do
+    it "will get show for valid user id" do
+
     end
   end
 
