9406cf4a3b4c207033acb831119239145e33ceb5
Jasmine <jasylop@gmail.com>
2020-11-23 07:50:10 -0800

    added outline for product model test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,8 +1,44 @@
 require "test_helper"
 
 describe Product do
-  describe "Validations" do
+  it "can be instantiated" do
 
+  end
+
+  it "will have the required fields" do
+
+  end
+  describe "Validations" do
+    it "must have a name" do
+
+    end
+    it "must have a description" do
+
+    end
+
+    it "must have a price" do
+
+    end
+
+    it "must have a price that is greater than 0" do
+
+    end
+
+    it "must have a price that is only an integer" do
+
+    end
+
+    it "must have stock" do
+
+    end
+
+    it "it must have a stock greater than 0" do
+
+    end
+
+    it "must have a user_id" do
+
+    end
   end
 
   describe "Relationships" do
@@ -28,7 +64,29 @@ describe Product do
   end
 
   describe "Custom Methods" do
+    describe "avg rating" do
+      it "returns nil if there are no ratings" do
 
+      end
+
+      it "returns the average of a number of ratings" do
+
+      end
+    end
+
+    describe "retire" do
+      it "will change active to true, if currently false" do
+
+      end
+
+      it "will change active to false, if currently true" do
+
+      end
+    end
+
+    describe "get top rated" do
+
+    end
   end
 
 
