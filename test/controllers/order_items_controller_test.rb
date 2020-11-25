381de40f2025a7bdd6d5f798c6ac8460d343eb05
dnsrocha <drocha@gmail.com>
2020-11-25 09:28:43 -0800

    fixing order_items controller tests

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe OrderItemsController do
+  before do
+    @fake_item_params = {quantity: 2, product_id: products(:japan).id, order_id: orders(:order1)}
+    @new_params = {quantity: 3, product_id: products(:disney).id, order_id: orders(:order1)}
+  end
+
+  describe "new" do
+    it 'can be instantiated' do
+      test_order_item = OrderItem.new(@new_params)
+      expect(test_order_item).must_be_kind_of OrderItem
+    end
+  end
 
   describe "create" do
+    it "can add new order item as guest" do
+      new_order_item = OrderItem.create(@fake_item_params)
+      expect(new_order_item).must_be_kind_of OrderItem
+
+    end
+
     it"adds new order item to current order" do
+      # fake_order_item = OrderItem.create(quantity: )
 
     end
 
@@ -16,14 +34,15 @@ describe OrderItemsController do
     end
   end
 
-  describe "destroy" do
-    before do
-      fake_item_params = {product_id: products(:fake_trip).id, quantity: 2}
-      post order_items_path, params: fake_item_params
 
-      @current_order = Order.find_by(id: session[:order_id])
-      @current_item = @current_order.order_items.first
-    end
+  describe "destroy" do
+    # before do
+    #   fake_item_params = {product_id: products(:fake_trip).id, quantity: 2}
+    #   post order_items_path, params: fake_item_params
+    #
+    #   @current_order = Order.find_by(id: session[:order_id])
+    #   @current_item = @current_order.order_items.first
+    # end
 
     it "deletes an order item with valid input" do
 
